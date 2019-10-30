592d32d4b096b1f732b1ce203b146caf653ab1b0
marcia <marciatainan@hotmail.com>
2019-10-30 16:20:42 -0300

    Adicionando rota pendente

--- src/controllers/tarefacontroller.js
+++ src/controllers/tarefacontroller.js
@@ -15,6 +15,11 @@ exports.getConcluido = (req, res)=>{
     res.status(200).send(result)
 }
 
+exports.getPendente = (req, res) => {
+    const result1 = tarefa.filter(tarefa => tarefa.concluido == "false")
+    res.status (200).send (result1)
+}
+
 exports.getColaborador = (req,res)=>{
     const colaborador = req.params.colaborador
     res.status(200).send(tarefa.find(tarefa => tarefa.colaborador == colaborador))
--- src/routes/tarefaRoute.js
+++ src/routes/tarefaRoute.js
@@ -6,6 +6,7 @@ const controller = require ("../controllers/tarefacontroller")
 
 router.get("/", controller.get)
 router.get("/concluido", controller.getConcluido)
+router.get ("/pendente", controller.getPendente)
 router.get("/:colaborador", controller.getColaborador)
 router.get("/:id", controller.getById)
 
