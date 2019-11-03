7771b10c9064ec711a97b3d3f071499d1f233517
marcia <marciatainan@hotmail.com>
2019-11-03 17:30:28 -0300

    verificando erros constados na tarefaRoute.js

--- src/routes/tarefaRoute.js
+++ src/routes/tarefaRoute.js
@@ -5,15 +5,9 @@ const controller = require("../controllers/tarefacontroller")
 
 
 router.get("/", controller.get)
-<<<<<<< HEAD
-router.get("/ordenar", controller.getOrdenar)
-router.get("/concluido", controller.getConcluido)
-router.get ("/pendente", controller.getPendente)
-=======
 router.get("/tarefaOrdenada", controller.getCompare)
 router.get("/concluido/filtrar", controller.getConcluido)
 router.get("/pendente/filtrar", controller.getPendente)
->>>>>>> 8f4d07239cb319ea21a9b873b35908728d408d4c
 router.get("/:id", controller.getById)
 router.get("/:colaborador/buscar", controller.getColaborador)
 
